f9563c0af5c85538891ac382120556ee533462fc
Hackbright Student <no-reply@hackbrightacademy.com>
2019-10-23 17:36:51 -0700

    Completeded Student Creation Form

---
+++ hackbright_web.py
@@ -0,0 +1,55 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template, redirect,url_for
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    html = render_template("student_info.html",
+                           first=first,
+                           last=last,
+                           github=github)
+    return html
+
+@app.route("/student-search")
+def get_student_form():
+    """Show form for searching for a student."""
+
+    return render_template("student_search.html")
+
+
+@app.route("/student-add-form")
+def student_add_form():
+    """Show form for searching for a student."""
+
+    return render_template("student-add.html")
+
+
+@app.route("/student-add", methods=['POST'])
+def student_add():
+    """Add a student."""
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    github = request.form.get('github')
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+    return render_template("add_confirmation.html", github=github)
+
+
+
+   
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True)
